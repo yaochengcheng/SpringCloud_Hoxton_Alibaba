899884eca9437d296b38b41612b513f875bb94c0
Cheng <yaocc915@163.com>
2021-02-19 10:39:49 +0800

    '环境搭建'

---
+++ cloud_pay_8001/src/main/java/com/atguigu/cloud/PayMainApplication.java
@@ -0,0 +1,16 @@
+package com.atguigu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ycc
+ * @created 2021/2/19 9:38
+ */
+@SpringBootApplication
+public class PayMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayMainApplication.class, args);
+    }
+}
